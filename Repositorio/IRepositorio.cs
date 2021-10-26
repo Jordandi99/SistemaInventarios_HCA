a73363e738c9d5e443f763ff7e7afc6c9e48ef7d
Jordandi99 <reclevel2k@gmail.com>
2021-10-26 13:06:17 -0500

    Agregar archivos de proyecto.

---
+++ Repositorio/IRepositorio.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq.Expressions;
+using PagedList;
+
+namespace Repositorio
+{
+    public interface IRepositorio : IDisposable
+    {
+        TEntity Create<TEntity>(TEntity newEntity) where TEntity : class;
+        bool Update<TEntity>(TEntity updateEntity) where TEntity : class;
+        bool Delete<TEntity>(TEntity deleteEntity) where TEntity : class;
+        TEntity FindEntity<TEntity>(Expression<Func<TEntity, bool>> critera) where TEntity : class;
+        IEnumerable<TEntity> FindEntitySet<TEntity>(Expression<Func<TEntity, bool>> critera) where TEntity : class;
+        IPagedList<TEntity> FindEntitySetPage<TEntity>(Expression<Func<TEntity, bool>> critera, Expression <Func<TEntity, string>> order, int page, int pageSize) where TEntity : class;
+    }
+}
